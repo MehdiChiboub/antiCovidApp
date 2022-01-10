3bb06a1091c2813885fd7a71c5fd006dfd5998fd
Soufianefd <soufianesd8@gmail.com>
2022-01-10 22:26:17 +0100

    pass pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,26 @@
-import 'package:anticovidapp/constants.dart';
+import 'package:anticovidapp/screens/pages/headPass.dart';
 import 'package:anticovidapp/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
-      title: 'Anti covid App',
+      title: 'Covid App',
       theme: ThemeData(
-        scaffoldBackgroundColor: kBackgroundColor,
-        primaryColor: kPrimaryColor,
-        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        scaffoldBackgroundColor: Color(0xFFF9F8FD),
+        primaryColor: Color(0xFF0C9869),
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: Color(0xFF3C4046)),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
